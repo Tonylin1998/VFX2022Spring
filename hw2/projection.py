6541283c4929ceb55658b66ea586871bfd42ab31
tnc <luke19981030@gmail.com>
2022-04-25 21:40:56 +0800

    projection

--- hw2/projection.py
+++ hw2/projection.py
@@ -9,14 +9,12 @@ def cylindrical_projection(images, focals):
     for i in range(h):
         for j in range(w):
             x = j - int(w/2)
-            y = h-1 - i
-            x1 = [int(focal*np.arctan(x/focal))+ int(w/2) for focal in focals]
-            y1 = [h-1 - int(focal*y/math.sqrt(focal*focal+x*x)) for focal in focals]
-            # print("(i,j):(",i,j,"), (i',j'):(",y1[0],x1[0],"), pixels: (",images[0][i,j])
+            y = h - 1 - i
             for f in range(len(focals)):
-                projection[f][y1[f],x1[f]] = images[f][i,j]
-    """
-    for i, image in enumerate(projection):
-        cv2.imwrite('data/parrington/Projection/image'+str(i)+'.jpg',image)
-    """
+                theta = np.arctan(x / focals[f])
+                height = y / np.sqrt(x ** 2 + focals[f] ** 2)
+                px = int(focals[f] * theta) + int(w / 2)
+                py = h - 1 - int(focals[f] * height)
+                projection[f][py, px] = images[f][i, j]
+
     return projection
